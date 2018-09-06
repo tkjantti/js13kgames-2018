c48fec5edb2ed702253f811a2a7fd29d87481449
Tero Jäntti <tkjantti@gmail.com>
2018-09-06 19:51:55 +0300

    Map texts refined for tutorial maps.

--- src/js/maps.js
+++ src/js/maps.js
@@ -49,7 +49,7 @@ const maps = [
     {
         online: Number.POSITIVE_INFINITY,
         offline: 0,
-        text: "THIS ONE IS EASY.",
+        text: "THESE WON'T HURT YOU.",
         data: [
             "                    ",
             "                    ",
@@ -71,7 +71,7 @@ const maps = [
     {
         online: 4000,
         offline: 10000,
-        text: "THEY WON'T HURT YOU. UNLESS...",
+        text: "UNLESS...",
         data: [
             "                          ",
             "                          ",
